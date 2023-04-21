4ab4b28d0966a7caf46d61494b990071f2ee3fdb
Achette <i.achette@live.com>
2023-04-21 14:24:55 -0300

    Feat: cria projeto PI e primeiro componente, instala dependencias

---
+++ src/components/header/index.tsx
@@ -0,0 +1,42 @@
+import { Image, Flex, Link, Button } from "@chakra-ui/react";
+import Logo from "../../assets/img/Logo.svg";
+
+export const Header = () => {
+  return (
+    <Flex
+      w="100%"
+      h="7.5rem"
+      bgColor="#3DCCB7"
+      color="#FFF"
+      justifyContent="space-evenly"
+      alignItems="center"
+    >
+      <Image w="20.625rem" h="4.5rem" src={Logo} />
+
+      <Flex
+        fontSize="1.25rem"
+        lineHeight="2.25rem"
+        fontWeight={400}
+        w="34.625rem"
+        justifyContent="space-between"
+      >
+        <Link>Início</Link>
+        <Link>Sobre</Link>
+        <Link>Depoimentos</Link>
+        <Link>Fale Conosco</Link>
+      </Flex>
+
+      <Button
+        w="12.25rem"
+        h="2.625rem"
+        borderRadius="2.5rem"
+        color="#FFF"
+        border="1px solid #FFF"
+        bgColor="#3DCCB7"
+        fontSize="0.875rem"
+      >
+        AGENDAR CONSULTA
+      </Button>
+    </Flex>
+  );
+};
