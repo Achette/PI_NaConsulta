2f59ca16e9b10c8bcd644e9103e9268af25dc628
Achette <i.achette@live.com>
2023-05-13 19:30:22 -0300

    Chore: inicia criação do segundo elemento do footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,5 @@
-import { Box, HStack, VStack, Text, Image } from "@chakra-ui/react";
+import { Box, HStack, VStack, Text, Image, Link } from "@chakra-ui/react";
+import { FooterForm } from "../footerForm";
 import Logo from "../../assets/img/Logo.png";
 import Facebook from "../../assets/img/Facebook.png";
 import Instagram from "../../assets/img/Instagram.png";
@@ -13,8 +14,8 @@ export const Footer = () => {
       bgColor="rgba(61, 204, 183, 0.16)"
       p="3.75rem 6.5rem"
     >
-      <VStack>
-        <HStack>
+      <VStack w="100%" h="100%" justifyContent="center" alignItems="center">
+        <HStack w="100%" justifyContent="space-between">
           <VStack w="21.55rem" h="10.75rem" justifyContent="space-between">
             <Image src={Logo} />
             <Text
@@ -33,6 +34,29 @@ export const Footer = () => {
               <Image src={LinkedIn} />
             </HStack>
           </VStack>
+
+          <Box
+            display="flex"
+            w="18.25rem"
+            h="11.5rem"
+            justifyContent="space-between"
+            border="1px solid red"
+          >
+            <VStack>
+              <Text>LINKS</Text>
+              <Link>Sobre Nós</Link>
+              <Link>Consultas</Link>
+              <Link>Depoimentos</Link>
+              <Link>Ajuda</Link>
+            </VStack>
+            <VStack>
+              <Text>SERVIÇOS</Text>
+              <Link>Agendamentos</Link>
+              <Link>Lembretes</Link>
+            </VStack>
+          </Box>
+
+          <FooterForm />
         </HStack>
         <Text>&copy; NaConsulta 2023. All rigths reserved.</Text>
       </VStack>
