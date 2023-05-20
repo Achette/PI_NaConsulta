d19330705a581ada9ff80acfdd5a183566b76436
Achette <i.achette@live.com>
2023-05-20 13:12:39 -0300

    Chore: inicia desenvolvimento da página de login

--- src/routes/users/index.tsx
+++ src/routes/users/index.tsx
@@ -4,9 +4,16 @@ import { Outlet } from "react-router-dom";
 
 export const UserAccess = () => {
   return (
-    <Flex flexDir="column" alignItems="center" maxH="100vh" w="100%" mt="1rem">
+    <Flex
+      flexDir="column"
+      alignItems="center"
+      maxH="100vh"
+      w="100%"
+      mt="1rem"
+      p="0 14.125rem"
+    >
       <Image src={Logo} w="20.625rem" h="4.5rem" mb="1rem" />
-      <Divider w="65%" h="0.05625rem" bg="rgba(0, 0, 0, 0.5)" mb="2.5rem" />
+      <Divider w="100%" h="0.05625rem" bg="rgba(0, 0, 0, 0.5)" mb="2.5rem" />
       <Outlet />
     </Flex>
   );
--- src/routes/users/login/index.tsx
+++ src/routes/users/login/index.tsx
@@ -1,11 +1,52 @@
-
+import { Box, Button, Heading, Input, VStack } from "@chakra-ui/react";
 
 export const Login = () => {
-
-    
   return (
-    <>
-      <p>Esta é a página de login</p>
-    </>
+    <Box border="1px solid red" w="100%" p="0 14.375rem">
+      <Heading
+        as="h1"
+        color="#004238"
+        fontWeight={600}
+        fontSize="2.5rem"
+        lineHeight="3.75rem"
+      >
+        Faça login na sua conta
+      </Heading>
+      <VStack>
+        <Input
+          type="text"
+          placeholder="E-mail"
+          w="37.5rem"
+          h="3.5rem"
+          bgColor="#F4F4F4"
+          border="0.9px solid #004238"
+          borderRadius="0.5rem"
+        />
+
+        <Input
+          type="text"
+          placeholder="Senha"
+          w="37.5rem"
+          h="3.5rem"
+          bgColor="#F4F4F4"
+          border="0.9px solid #004238"
+          borderRadius="0.5rem"
+        />
+
+        <Button
+          w="37.5rem"
+          h="3.5rem"
+          bgColor="#004238"
+          borderRadius="0.5rem"
+          color="#FFF"
+          fontSize="1rem"
+          fontWeight={500}
+          lineHeight="1.5rem"
+          cursor="pointer"
+        >
+          Entrar
+        </Button>
+      </VStack>
+    </Box>
   );
 };
