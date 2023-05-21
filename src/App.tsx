cc93dc0e801a0a236c604bdc158cff3327a24aa2
Achette <i.achette@live.com>
2023-05-20 21:26:49 -0300

    Feat: adiciona nova rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage, UserAccess } from "./routes";
 import { Login } from "./routes/users/login";
+import { Signin } from "./routes/users/signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<UserAccess />}>
+        <Route path="/access" element={<UserAccess />}>
           <Route index element={<Login />} />
+          <Route path="/access/signin" element={<Signin />} />
         </Route>
       </Routes>
     </BrowserRouter>
