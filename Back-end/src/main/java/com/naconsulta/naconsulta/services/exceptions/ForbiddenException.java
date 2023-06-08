cdd5fed2e31bf836edbea383db83b7b9d95c003c
Achette <i.achette@live.com>
2023-06-08 12:29:16 -0300

    CI: move backend para repositório separado

--- Back-end/src/main/java/com/naconsulta/naconsulta/services/exceptions/ForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package com.naconsulta.naconsulta.services.exceptions;
-
-public class ForbiddenException extends RuntimeException {
-
-    public ForbiddenException(String msg) {
-        super(msg);
-    }
-}
--- Back-end/src/main/java/com/naconsulta/naconsulta/services/exceptions/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.naconsulta.naconsulta.services.exceptions;
-
-public class UnauthorizedException extends RuntimeException {
-
-    public UnauthorizedException(String msg) {
-        super(msg);
-    }
-}
