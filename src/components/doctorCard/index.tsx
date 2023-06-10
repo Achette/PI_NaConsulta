328e2767fd441c28fe0b2a43fb10affb3804d11c
Victormoroo <victorhugo0609@outlook.com>
2023-06-10 00:25:39 -0300

    chore: importacao do DoctorSchedule

--- src/components/doctorCard/index.tsx
+++ src/components/doctorCard/index.tsx
@@ -1,20 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { DoctorInfo } from "../doctorInfo";
 import { mock } from "../../mock";
+import { DoctorSchedule } from "../doctorSchedule";
 
 const { doctor } = mock;
 export const DoctorCard = () => {
   return (
-    <Box w="68.25rem" h="18.75rem" bgColor="#F7F9F9" p="1.5rem">
-      <DoctorInfo
-        name={doctor.name}
-        specialty={doctor.specialty}
-        address={doctor.address}
-        cep={doctor.cep}
-        neighborhood={doctor.neighborhood}
-        room={doctor.room}
-        city={doctor.city}
-      />
-    </Box>
+    <Flex w="68.25rem" h="18.75rem" bgColor="#F7F9F9" p="1.5rem" >
+      <Flex flex={1}>
+          <DoctorInfo
+            name={doctor.name}
+            specialty={doctor.specialty}
+            address={doctor.address}
+            cep={doctor.cep}
+            neighborhood={doctor.neighborhood}
+            room={doctor.room}
+            city={doctor.city}
+          />
+      </Flex>
+      <Flex flex={1}>
+          <DoctorSchedule />
+      </Flex>
+    </Flex>
   );
 };
