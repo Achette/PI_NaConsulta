3d531b1a33c2811674b2f7ad11906b28add08f46
unknown <drjunior988@hotmail.com>
2023-06-09 22:33:35 -0300

    feat: Criação mock

---
+++ src/mock/index.ts
@@ -0,0 +1,14 @@
+
+
+export const mock = {
+    doctor: {
+      name: "Dra Rebeca Carvalho Goncaçalves",
+      specialty: "Psiquiatra",
+      address:"Rua Heitor Magalhões, 420",
+      neighborhood:"Vila Aparecida",
+      city: "Franca - Sp",
+      cep: "14400-0000",
+      room: "Sala 8",
+
+    },
+  };
