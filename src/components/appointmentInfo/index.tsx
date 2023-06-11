b91ee13258c71be83c5600bb9e742fa1f47608ab
Achette <i.achette@live.com>
2023-06-11 12:03:21 -0300

    chore: inicia criação de componente appointment info

--- src/components/appointmentInfo/index.tsx
+++ src/components/appointmentInfo/index.tsx
@@ -1,11 +1,37 @@
-import { Flex, Heading } from "@chakra-ui/react"
-
+import { FormLabel, Heading, Radio, RadioGroup, Stack, VStack } from "@chakra-ui/react";
+import { AppointmentInput } from "../appointmentInput";
+import { AppointmentInputRadio } from "../appointmentRadio";
 
 export const AppointmentInfo = () => {
+  return (
+    <VStack
+      w="37.5rem"
+      h="35.5rem"
+      border="1px solid red"
+      alignItems="flex-start"
+    >
+      <Heading
+        as="h2"
+        color="#004238"
+        fontSize="2rem"
+        fontWeight={600}
+        lineHeight="3rem"
+      >
+        Opções da Consulta
+      </Heading>
 
-    return (
-        <Flex>
-            <Heading as="h2">Opções da Consulta</Heading>
-        </Flex>
-    )
-}
+      <AppointmentInput
+        label="Tipo de consulta *"
+        value="Consulta Psiquiátrica"
+      />
+      <AppointmentInput label="Valor *" value="R$ 500,00" />
+
+      <AppointmentInput
+        label="Convênio Médico *"
+        value="Este especialista não aceita convênio médico"
+      />
+
+      <AppointmentInputRadio />
+    </VStack>
+  );
+};
