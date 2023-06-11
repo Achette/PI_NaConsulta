7c821a9506047a1fc421b8354abdbb3520717da4
Victormoroo <victorhugo0609@outlook.com>
2023-06-10 22:39:39 -0300

    feat: adicionado margin-bottom

--- src/components/doctorCard/index.tsx
+++ src/components/doctorCard/index.tsx
@@ -1,26 +1,26 @@
-import { Flex } from "@chakra-ui/react";
-import { DoctorInfo } from "../doctorInfo";
-import { mock } from "../../mock";
-import { DoctorSchedule } from "../doctorSchedule";
+import { Flex } from "@chakra-ui/react"
+import { DoctorInfo } from "../doctorInfo"
+import { mock } from "../../mock"
+import { DoctorSchedule } from "../doctorSchedule"
 
-const { doctor } = mock;
+const { doctor } = mock
 export const DoctorCard = () => {
   return (
-    <Flex w="68.25rem" h="18.75rem" bgColor="#F7F9F9" p="1.5rem" >
+    <Flex w="68.25rem" h="18.75rem" bgColor="#F7F9F9" p="1.5rem" mb="0.5rem">
       <Flex flex={1}>
-          <DoctorInfo
-            name={doctor.name}
-            specialty={doctor.specialty}
-            address={doctor.address}
-            cep={doctor.cep}
-            neighborhood={doctor.neighborhood}
-            room={doctor.room}
-            city={doctor.city}
-          />
+        <DoctorInfo
+          name={doctor.name}
+          specialty={doctor.specialty}
+          address={doctor.address}
+          cep={doctor.cep}
+          neighborhood={doctor.neighborhood}
+          room={doctor.room}
+          city={doctor.city}
+        />
       </Flex>
       <Flex flex={1}>
-          <DoctorSchedule />
+        <DoctorSchedule />
       </Flex>
     </Flex>
-  );
-};
+  )
+}
