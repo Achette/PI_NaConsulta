29e215bd948f78efaabada48f7f2dc09779c5137
Achette <i.achette@live.com>
2023-06-12 17:21:28 -0300

    feat: adiciona container para notificação toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css'
 import {
   LandingPage,
   UserAccess,
@@ -14,23 +16,29 @@ import {
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/access" element={<UserAccess />}>
-          <Route index element={<Login />} />
-          <Route path="/access/signin" element={<Signin />} />
-        </Route>
-        <Route path="/search" element={<SearchDoctors />} />
-        <Route path="/schedule" element={<Schedule />}>
-          <Route element={<Appointment />}>
-            <Route index element={<Info />} />
-            <Route path="/schedule/userinfo" element={<UserInfo />} />
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/access" element={<UserAccess />}>
+            <Route index element={<Login />} />
+            <Route path="/access/signin" element={<Signin />} />
           </Route>
-        </Route>
-            <Route path="/schedule/userinfo/completedinfo" element={<CompletedInfo />}></Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/search" element={<SearchDoctors />} />
+          <Route path="/schedule" element={<Schedule />}>
+            <Route element={<Appointment />}>
+              <Route index element={<Info />} />
+              <Route path="/schedule/userinfo" element={<UserInfo />} />
+            </Route>
+          </Route>
+          <Route
+            path="/schedule/userinfo/completedinfo"
+            element={<CompletedInfo />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 
