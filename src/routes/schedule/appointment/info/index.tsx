a95993d5b705642f1250622537152911eafecd6c
Victormoroo <victorhugo0609@outlook.com>
2023-06-12 00:20:15 -0300

    refact: mudanca na formatação

--- src/routes/schedule/appointment/info/index.tsx
+++ src/routes/schedule/appointment/info/index.tsx
@@ -1,10 +1,10 @@
-import { Flex } from "@chakra-ui/react";
-import { AppointmentInfo } from "../../../../components";
+import { Flex } from "@chakra-ui/react"
+import { AppointmentInfo } from "../../../../components"
 
 export const Info = () => {
   return (
     <Flex w="37.5rem" justifyContent="space-between">
       <AppointmentInfo />
     </Flex>
-  );
-};
+  )
+}
